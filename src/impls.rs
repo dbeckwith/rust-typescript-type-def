8f5163beb6f6494d122d4e8a816d2becb2221423
Daniel Beckwith <djbsnx@gmail.com>
2021-06-16 09:02:31 -0400

    More tuple impls

--- src/impls.rs
+++ src/impls.rs
@@ -105,14 +105,24 @@ macro_rules! impl_tuple {
     };
 }
 
-impl_tuple!(T0);
-impl_tuple!(T0, T1);
-impl_tuple!(T0, T1, T2);
-impl_tuple!(T0, T1, T2, T3);
-impl_tuple!(T0, T1, T2, T3, T4);
-impl_tuple!(T0, T1, T2, T3, T4, T5);
-impl_tuple!(T0, T1, T2, T3, T4, T5, T6);
-impl_tuple!(T0, T1, T2, T3, T4, T5, T6, T7);
+impl_tuple!(T1);
+impl_tuple!(T1, T2);
+impl_tuple!(T1, T2, T3);
+impl_tuple!(T1, T2, T3, T4);
+impl_tuple!(T1, T2, T3, T4, T5);
+impl_tuple!(T1, T2, T3, T4, T5, T6);
+impl_tuple!(T1, T2, T3, T4, T5, T6, T7);
+impl_tuple!(T1, T2, T3, T4, T5, T6, T7, T8);
+impl_tuple!(T1, T2, T3, T4, T5, T6, T7, T8, T9);
+impl_tuple!(T1, T2, T3, T4, T5, T6, T7, T8, T9, T10);
+impl_tuple!(T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11);
+impl_tuple!(T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12);
+impl_tuple!(T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13);
+impl_tuple!(T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14);
+impl_tuple!(T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15);
+impl_tuple!(
+    T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15, T16
+);
 
 impl<T, const N: usize> TypeDef for [T; N]
 where
